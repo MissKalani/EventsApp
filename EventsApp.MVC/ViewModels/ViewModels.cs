8af2f6afbaeb5d8f9c538614ce6a55fb8f55cb62
mae laurene suralta <mlsuralta@hotmail.com>
2015-11-10 16:52:47 +0100

    Changed to a single view model class.
    Also deleted some unnecessary views.

---
+++ EventsApp.MVC/ViewModels/ViewModels.cs
@@ -0,0 +1,20 @@
+﻿using EventsApp.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventsApp.MVC.ViewModels
+{
+    public class ViewModels
+    {
+        public RegisterUserViewModel RegisterUserViewModel { get; set; }
+        public LoginUserViewModel LoginUserViewModel { get; set; }
+        public ManageUserViewModel ManageUserViewModel { get; set; }
+        public ConfirmViewModel ConfirmViewModel { get; set; }
+        public CreateViewModel CreateViewModel { get; set; }
+        public ViewEvent ViewEvent { get; set; }
+        public List<Event> Events { get; set; }
+        public Event Event { get; set; }
+    }
+}
