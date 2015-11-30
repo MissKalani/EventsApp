6e1ede355faaa84d29a8cc844375d444bb17c4e1
mae laurene suralta <mlsuralta@hotmail.com>
2015-11-30 11:52:32 +0100

    design changes

--- EventsApp.MVC/App_Start/BundleConfig.cs
+++ EventsApp.MVC/App_Start/BundleConfig.cs
@@ -15,13 +15,13 @@ namespace EventsApp.MVC
             //bundles.Add(new ScriptBundle("~/bundles/js/modernizr").Include("~/Scripts/modernizr-{version}.js"));
             //bundles.Add(new ScriptBundle("~/bundles/js/jquery").Include("~/Scripts/jquery-{version}.min.js"));
             bundles.Add(new ScriptBundle("~/bundles/js/modernizr").Include("~/Scripts/modernizr-2.6.2.js"));
-            bundles.Add(new ScriptBundle("~/bundles/js/jquery").Include("~/Scripts/jquery-1.10.2.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/js/jquery").Include("~/Scripts/jquery-1.11.3.min.js"));
             bundles.Add(new ScriptBundle("~/bundles/js/bootstrap").Include("~/Scripts/bootstrap.min.js"));
             bundles.Add(new ScriptBundle("~/bundles/js/bootstrap-datetimepicker").Include("~/Scripts/moment.min.js", "~/Scripts/bootstrap-datetimepicker.min.js"));
             bundles.Add(new ScriptBundle("~/bundles/js/event").Include("~/Scripts/event.js"));
             bundles.Add(new ScriptBundle("~/bundles/js/jquery-ui").Include("~/Scripts/jquery-ui-1.11.4.min.js"));
             bundles.Add(new StyleBundle("~/bundles/css/style").Include("~/Content/bootstrap.min.css", "~/Content/bootstrap-datetimepicker.min.css", "~/Content/Site.css", "~/Content/MapContextMenu.css"));
-            bundles.Add(new ScriptBundle("~/bundles/js/dataTables").Include("~/Scripts/jquery-1.11.3.min.js", "~/Scripts/jquery.dataTables.min.js", "~/js/Scripts/dataTables.bootstrap.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/js/dataTables").Include("~/Scripts/jquery.dataTables.min.js", "~/js/Scripts/dataTables.bootstrap.min.js"));
         }
     }
 }
